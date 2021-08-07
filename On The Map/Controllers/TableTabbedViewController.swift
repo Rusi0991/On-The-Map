98bb410d6686b4cadbf0b3996b93c61c4d2657ca
Ruslan Ismayilov <ruslanismayilov484@gmail.com>
2021-08-07 12:11:51 -0700

    TableViewDelegate and TableViewData source protocols and cellForRowAtIndexPath, numberOfRowInSections functions added

--- On The Map/Controllers/TableTabbedViewController.swift
+++ On The Map/Controllers/TableTabbedViewController.swift
@@ -7,24 +7,22 @@
 
 import UIKit
 
-class TableTabbedViewController: UIViewController {
+class TableTabbedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableTabbedViewCell")!
+        return cell
     }
-    */
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
 
 }
