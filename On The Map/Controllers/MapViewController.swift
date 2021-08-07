c7e26af2d1b244f2923f0b1eceaf5ac4584dce75
Ruslan Ismayilov <ruslanismayilov484@gmail.com>
2021-08-07 11:33:24 -0700

    Map View and Table View added

---
+++ On The Map/Controllers/MapViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewController.swift
+//  On The Map
+//
+//  Created by Ruslan Ismayilov on 8/7/21.
+//
+
+import UIKit
+import MapKit
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
