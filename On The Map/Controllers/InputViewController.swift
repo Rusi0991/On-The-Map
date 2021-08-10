98e831a8e479f88c009d16d21c766dfd5310c682
Ruslan Ismayilov <ruslanismayilov484@gmail.com>
2021-08-09 18:07:51 -0700

    Input View Controller UI created

---
+++ On The Map/Controllers/InputViewController.swift
@@ -0,0 +1,29 @@
+//
+//  InputViewController.swift
+//  On The Map
+//
+//  Created by Ruslan Ismayilov on 8/9/21.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- On The Map/Controllers/Login ViewController.swift
+++ On The Map/Controllers/Login ViewController.swift
@@ -7,20 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    // Outlets
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
+    // Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        emailTextField.delegate = self
+        passwordTextField.delegate = self
+        hideKeyboardWhenTappedAround()
     }
 
     @IBAction func loginTapped(_ sender: Any) {
+        performSegue(withIdentifier: "toTabViewController", sender: (Any).self)
     }
     
     @IBAction func signUpButtonTapped(_ sender: Any) {
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+           let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+           tap.cancelsTouchesInView = false
+           view.addGestureRecognizer(tap)
+       }
+    
+    @objc func dismissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTextField{
+        textField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        }
+        return true
     }
 }
 
