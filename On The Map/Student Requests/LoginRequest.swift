ce2f2249bbb322c8ac7deacb5c5ee3dadc3dc653
Rusi0991 <ruslanismayilov484@gmail.com>
2021-08-17 18:34:05 -0700

    LoginRequest file created

---
+++ On The Map/Student Requests/LoginRequest.swift
@@ -0,0 +1,12 @@
+//
+//  LoginRequest.swift
+//  On The Map
+//
+//  Created by Ruslan Ismayilov on 8/17/21.
+//
+
+import Foundation
+struct LoginRequest {
+    let username : String
+    let password : String
+}
