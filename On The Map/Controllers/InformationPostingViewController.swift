df608dbe7f09ee62ed5f6172e75394c626aca2c1
Rusi0991 <ruslanismayilov484@gmail.com>
2021-08-27 20:14:54 -0700

    subscribeToKeyBoardNotifications and unsubscribeToKeybordNotifications functions are added

--- On The Map/Controllers/InformationPostingViewController.swift
+++ On The Map/Controllers/InformationPostingViewController.swift
@@ -25,6 +25,11 @@ class InformationPostingViewController: UIViewController, UITextFieldDelegate {
     override func viewWillAppear(_ animated: Bool) {
         tabBarController?.tabBar.isHidden = true
         hideKeyboardWhenTappedAround()
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
     }
     
     
@@ -96,5 +101,38 @@ class InformationPostingViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(_ notification:Notification) {
+        
+        if locationTextField.isEditing || linkTextField.isEditing {
+            view.frame.origin.y = (-1)*getKeyboardHeight(notification)
+        }
+    }
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        
+        view.frame.origin.y = 0
+    }
     
 }
--- On The Map/Controllers/Login ViewController.swift
+++ On The Map/Controllers/Login ViewController.swift
@@ -29,6 +29,11 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.tabBarController?.tabBar.isHidden = true
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        unsubscribeFromKeyboardNotifications()
     }
 
     @IBAction func loginTapped(_ sender: Any) {
@@ -108,6 +113,38 @@ class ViewController: UIViewController, UITextFieldDelegate {
         loginButton.isEnabled = !loggingIn
         signUpButton.isEnabled = !loggingIn
         activityIndicator.isHidden = !loggingIn
+    }
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc func keyboardWillShow(_ notification:Notification) {
+        
+        if passwordTextField.isEditing || emailTextField.isEditing {
+            view.frame.origin.y = (-1)*getKeyboardHeight(notification)
+        }
+    }
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        
+        view.frame.origin.y = 0
     }
     
 }
